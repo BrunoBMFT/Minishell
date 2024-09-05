17075171b2109227cd3334eefc6194aab1494eea
Bruno Miguel Fernandes Tender <brfernan@c2r5s2.42lisboa.com>
2024-09-05 20:47:26 +0100

    backup

---
+++ work_minishell/srcs/aux/signal.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/22 03:18:24 by bruno             #+#    #+#             */
+/*   Updated: 2024/09/05 14:03:32 by brfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+void	sigquit(int sig)
+{
+	(void)sig;
+	write(1, "Quit:\n", 7);
+}
+
+void	setup_sigquit_handler(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGQUIT, &sa, NULL);
+}
+
+// Example signal handlers
+void	handle_signal_main(int sig)
+{
+	(void)sig;
+	write(1, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+void	handle_signal_heredoc(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n", 1);
+	close(STDIN_FILENO);
+}
+
+void	handle_signal_child(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n", 1);
+}
---
+++ work_minishell/srcs/executor/process.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfernan <brfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 19:13:31 by bruno             #+#    #+#             */
+/*   Updated: 2024/09/05 15:44:01 by brfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int	child_process(t_jobs *job, t_env env)
+{
+	pid_t	pid;
+	int		fd[2];
+	int		status = 0;
+
+	if (job->job && job->job[0] && (ft_strcmp(job->job[0], "cd")) == 0)
+		return (caught_cd(job, env.env));
+	pipe(fd);
+	pid = new_fork();
+	if (pid == 0)
+	{
+		close(fd[READ]);
+		if (job->next && job->next->type == PIPE)//might not need this check
+			dup2(fd[WRITE], STDOUT_FILENO);//error check
+		close(fd[WRITE]);
+		if (try_builtins(job, env, true) == 200)
+			execute_job(job->job, env.env);
+	}
+	close(fd[WRITE]);
+	dup2(fd[READ], STDIN_FILENO);//error check
+	close(fd[READ]);
+	waitpid(pid, &status, 0);
+	return (WEXITSTATUS(status));
+}
+
+int	simple_process(t_jobs *job, t_env env)//recieve pipe to decide exit?
+{
+	pid_t	pid;
+	int	status;
+
+	if (job->job && job->job[0] && (ft_strcmp(job->job[0], "cd")) == 0)
+		return (caught_cd(job, env.env));
+	status = try_builtins(job, env, false);
+	if (status != 200)
+		return (status);
+	//has to test if executable_path exists here
+	pid = new_fork();
+	if (pid == 0)
+	{
+		execute_job(job->job, env.env);
+	}
+	waitpid(pid, &status, 0);
+	return (WEXITSTATUS(status));
+}
+
+void	panic(char *s)
+{
+	ft_putendl_fd(s, 2);
+	exit(1);
+}
+
+int	new_fork(void)
+{
+	int	pid;
+
+	pid = fork();
+	if (pid == -1)
+		panic("fork");// TODO stop using panic
+	return (pid);
+}
