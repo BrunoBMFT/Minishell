109afdf37be9ca51b86f35c56d6f2a84be31ed6b
Yohan Cantin <ycantin@c2r6s12.42lisboa.com>
2024-10-13 16:05:29 +0100

    leaks using export

---
+++ srcs/builtins/export.c
@@ -0,0 +1,174 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycantin <ycantin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 18:15:45 by bruno             #+#    #+#             */
+/*   Updated: 2024/10/12 15:10:37 by ycantin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+// bool	is_in_env(char *to_add, char **env)
+// {
+// 	int	i;
+// 	char	*temp1 = NULL;
+// 	char	*temp2 = NULL;
+
+// 	if (!to_add || !env)
+// 		return false;
+// 	i = 0;
+// 	temp1 = ft_substr(to_add, 0, len_to_equal(to_add));
+// 	while (env[i])
+// 	{
+// 		temp2 = ft_substr(env[i], 0, len_to_equal(env[i]));
+// 		if (ft_strcmp(temp1, temp2) == 0)
+// 		{
+//             free(temp1);
+//             free(temp2);
+// 			//free(env[i]);
+// 			env[i] = ft_strdup(to_add);
+// 			return (true);
+// 		}
+//         free(temp2);
+// 		i++;
+// 	}
+// 	return (free(temp1), false);
+// }
+
+bool is_in_env(char *to_add, char **env)
+{
+	int	i;
+	char	*temp1 = NULL;
+	char	*temp2 = NULL;
+
+	if (!to_add || !env)
+		return false;
+	i = 0;
+    temp1 = ft_substr(to_add, 0, len_to_equal(to_add));
+    while (env[i]) {
+        temp2 = ft_substr(env[i], 0, len_to_equal(env[i]));
+        if (ft_strcmp(temp1, temp2) == 0) {
+            free(temp1);
+            free(temp2); // Free temp2 here
+            free(env[i]); // Free old env[i]
+            env[i] = ft_strdup(to_add); // Ensure to free the old env[i] before overwriting
+            return (true);
+        }
+        free(temp2); // Free temp2 in each loop iteration
+        i++;
+    }
+    free(temp1); // Ensure temp1 is freed at the end
+    return false;
+}
+
+
+bool	parse_export(char *str, int n)//error messages
+{
+	int	i;
+	if (!str || !ft_isalphaund(str[0]))
+		return (false);
+	i = 0;
+	while (i < n)
+	{
+		if (!ft_isalnumund(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+int	export_no_execd(char **env)
+{
+	int		i;
+	char	*temp1;
+	char	*temp2;
+
+	i = 0;
+	while (env[i])
+	{
+		temp1 = ft_substr(env[i], 0, len_to_equal(env[i]) + 1);//error check
+		temp2 = ft_substr(env[i], len_to_equal(env[i]) + 1, ft_strlen(env[i]));//error check
+		ft_printf("declare -x %s\"%s\"\n", temp1, temp2);
+		free (temp1);
+		free (temp2);
+		i++;
+	}
+	return (0);
+}
+
+int	caught_export(t_jobs *job, t_env *env)//fix export var =value (the space)
+{
+	int	status;
+
+	if (!job->job[1])
+		return (export_no_execd(env->env));//fix
+	job->job++;
+	status = 0;
+
+	int oldenv_count = ft_split_wordcount(env->env);
+	int var_count = ft_split_wordcount(job->job);
+	char **new_env = ft_calloc(sizeof(char *), var_count + oldenv_count + 1);
+	
+	int i = 0;
+	while (env->env[i])
+	{
+		new_env[i] = ft_strdup(env->env[i]);//free env[i]
+		i++;
+	}
+	while (*job->job)
+	{
+		if (!parse_export(*job->job, len_to_equal(*job->job)))
+		{
+			ft_printf_fd(2, "minishell: export: '%s': not a valid identifier\n", *job->job);
+			status = 1;//check exit code
+		}
+		else
+		{
+			if (!is_in_env(*job->job, env->env))
+				new_env[i] = ft_strdup(*job->job);
+			// else
+    		// 	free(*job->job);
+		}
+		i++;
+		job->job++;
+	}
+	new_env[i] = NULL;
+
+	//free old env
+	i = 0;
+	while (env->env[i])
+	{
+		free (env->env[i]);
+		i++;
+	}
+	free (env->env);
+
+	//sending to new env
+	env->env = ft_calloc(sizeof(char *), ft_split_wordcount(new_env) + 1);
+	i = 0;
+	while (new_env[i])
+	{
+		env->env[i] = ft_strdup(new_env[i]);
+		free (new_env[i]);
+		i++;
+	}
+	env->env[i] = NULL;
+	
+	free(new_env);
+	
+/* 	//freeing new_env
+	i = 0;
+	while (new_env[i])
+	{
+		free (new_env[i]);
+		i++;
+	}
+	free (new_env); */
+
+
+	return (status);
+}
---
+++ srcs/executor/executor.c
@@ -0,0 +1,170 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ycantin <ycantin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 17:26:33 by bruno             #+#    #+#             */
+/*   Updated: 2024/10/10 16:21:01 by ycantin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+// int		choose_input(t_jobs **jobs)
+// {
+// 	t_jobs *copy;
+// 	int 	input;
+// 	copy = *jobs;
+// 	while (copy)
+// 	{
+// 		if (handle_heredoc(copy) < 0)
+// 			return (-1);
+// 	}
+// }
+
+int	executor_input(t_jobs *job, int *status)
+{
+	int	redirected_input;
+
+	redirected_input = open(job->input, O_RDONLY);
+	if (redirected_input == -1)
+	{
+		ft_printf_fd(2, "bash: %s: No such file or directory\n", job->input);//change to perror?
+		*status = 127;
+		return (-1);
+	}
+	dup2(redirected_input, STDIN_FILENO);//wrong input + pipe not working
+	close(redirected_input);
+	return (0);
+}
+
+int	executor_output(t_jobs *job, int *status)
+{
+	int	redirected_output;
+
+	if (job->append)
+		redirected_output = open(job->output, O_CREAT | O_APPEND | O_RDWR, 0644);
+	else
+	{
+		if (access(job->output, F_OK) == 0)
+			remove(job->output);
+		redirected_output = open(job->output, O_CREAT | O_RDWR, 0644);
+	}
+
+	if (redirected_output < 0) 
+	{
+		ft_printf_fd(2, "bash: %s: No such file or directory\n", job->input);
+		*status = 127;
+		return (-1);
+	}
+	dup2(redirected_output, STDOUT_FILENO);
+	close(redirected_output);//else
+	return (0);
+}
+
+void	start_executor(t_jobs *job, t_env *env)
+{
+	int 	saved_stdin;
+	int 	saved_stdout;
+	int 	redirected_output;
+	signal(SIGINT, handle_signal_child);
+	signal(SIGQUIT, sigquit);
+	
+	saved_stdin = dup(STDIN_FILENO);
+	saved_stdout = dup(STDOUT_FILENO);
+	while (job)
+	{
+		//expanding
+		if (job->job)
+			modify_array(job->job, env);
+		
+		//redirections
+		if (job->input)
+		{
+			// if (ft_strcmp(job->input, "/dev/null") == 0)
+			// {
+			// 	job = job->next;
+			// 	continue ;
+			// }
+			if (executor_input(job, &env->status) < 0)
+			{
+//				job = job->next;
+//				continue;
+//the skipping cant happen here, cause the pipe needs to happen to save stdout to fd read
+// ! what the fuck
+			}
+		}
+		if (job->output)
+        {
+			if (executor_output(job, &env->status) < 0)
+			{
+//				job = job->next;//check if there is a job next?
+//				continue;//continue???
+			}
+        }
+		//pipes
+		if (job->next && job->next->type == PIPE)// < sdakaskddask cat | wc
+		{
+			env->status = child_process(job, env);
+			job = job->next->next;
+			job->piped = true;
+			continue;
+		}
+
+
+
+
+
+		
+		//executing jobs
+		else if (job->job && job->job[0] && job->piped)
+			env->status = child_process(job, env);//last proc
+		else if (job->job && job->job[0])
+			env->status = simple_process(job, env);
+
+
+
+		//resets
+		dup2(saved_stdin, STDIN_FILENO);
+		dup2(saved_stdout, STDOUT_FILENO);
+		if (job->heredoc_file && access(job->heredoc_file, F_OK) == 0)
+			remove(job->heredoc_file);
+
+
+
+
+
+
+
+		//operators
+		if (job->next && job->next->type == AND)
+		{
+			job = job->next->next;
+			job->piped = false;
+		}
+		else if (job->next && job->next->type == OR)
+		{
+			if (env->status == 0)
+			{
+				while(job->next && job->next->type == OR)
+					job = job->next->next;
+				if (job->next)
+					job = job->next->next;
+				else
+					job = job->next;
+			}
+			else
+				job = job->next;
+			job->piped = false;
+		}
+		else
+			job = job->next;
+	}
+	if (access(".heredoc", F_OK) == 0)
+		remove(".heredoc");
+	close(saved_stdin);//if exit is called, it will exit before closing this
+	close(saved_stdout);//if exit is called, it will exit before closing this
+	return ;
+}
